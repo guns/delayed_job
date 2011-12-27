f8d3cf75f41026b33ee1766e8b9acbd3d89ddaa1
guns <self@sungpae.com>
2011-12-27 16:54:16 -0600

    Patch DJ's monkey patch of YAML's monkey patch for Ruby 1.9.3

--- lib/delayed/yaml_ext.rb
+++ lib/delayed/yaml_ext.rb
@@ -27,7 +27,7 @@ end
 
 class Class
   yaml_as "tag:ruby.yaml.org,2002:class"
-  remove_method :to_yaml # use Module's to_yaml
+  remove_method :to_yaml if respond_to? :to_yaml and method(:to_yaml).owner == self # use Module's to_yaml
 end
 
 class Struct
